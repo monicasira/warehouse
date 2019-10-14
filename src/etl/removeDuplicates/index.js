8bf63e9841dfbd370929739723116ca6ba386f51
bellvat <msirathanarun@gmail.com>
2019-10-14 16:30:21 +0700

    fix remove dup again

--- src/etl/removeDuplicates/index.js
+++ src/etl/removeDuplicates/index.js
@@ -57,19 +57,14 @@ function main() {
     const [rows] = await job.getQueryResults();
     console.log(`Job ${job.id} deleted duplicates with row 1.`);
 
-    const [table1] = await bigquery
-      .dataset(datasetId)
-      .table(tableId)
-      .get();
-    const destinationTableRef1 = table1.metadata.tableReference;
-
-    const query2 = `SELECT * except(row_number) FROM \`data-warehouse-srichand.${datasetId}.${tableId}\``;
+    // remove row number columm
+    const query2 = `SELECT no_dup.* except (row_number) FROM \`data-warehouse-srichand.${datasetId}.${tableId}\` as no_dup`;
 
     const options2 = {
       query: query2,
       location: 'asia-east2',
       writeDisposition: 'WRITE_TRUNCATE',
-      destinationTable: destinationTableRef1,
+      destinationTable: destinationTableRef,
     };
 
     // Run the query as a job
