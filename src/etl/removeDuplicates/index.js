6016d9b10ed5a93ac881cda41fdc37dcb7ddf705
bellvat <msirathanarun@gmail.com>
2019-10-14 20:09:19 +0700

    update to production

--- src/etl/removeDuplicates/index.js
+++ src/etl/removeDuplicates/index.js
@@ -17,7 +17,7 @@ function main() {
   const {BigQuery} = require('@google-cloud/bigquery');
   // Instantiate client
   const bigquery = new BigQuery();
-  const datasetId = 'ecomm_test';
+  const datasetId = 'ecomm_production';
   const tableId = 'transactions';
   async function removeDuplicates() {
 
@@ -51,11 +51,11 @@ function main() {
 
     // Run the query as a job
     const [job] = await bigquery.createQueryJob(options);
-    console.log(`Job ${job.id} remove duplicate started.`);
+    console.log(`Job ${job.id} start remove duplicate.`);
 
     // Wait for the query to finish
     const [rows] = await job.getQueryResults();
-    console.log(`Job ${job.id} delete duplicate from transactions table.`);
+    console.log(`Job ${job.id} complete delete duplicate from transactions table.`);
 
     // remove row number columm
     const query2 = `SELECT * except (rn) FROM \`data-warehouse-srichand.${datasetId}.${tableId}\``;
@@ -72,7 +72,7 @@ function main() {
     console.log(`Job ${job2.id} start to delete column row_number(rn)`);
 
     const [rows2] = await job2.getQueryResults();
-    console.log(`Job ${job2.id} delete column row_number(rn).`);
+    console.log(`Job ${job2.id} complete delete column row_number(rn).`);
   }
   // [END bigquery_add_column_load_append]
   removeDuplicates();
