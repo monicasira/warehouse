12bbc53cca7ca4d3ea528ed4411cdc44e80feac9
bellvat <msirathanarun@gmail.com>
2019-10-16 16:03:23 +0700

    update async function in remove duplicate

--- src/etl/removeDuplicates/index.js
+++ src/etl/removeDuplicates/index.js
@@ -27,35 +27,41 @@ async function main() {
       .table(tableId)
       .get();
     const destinationTableRef = table.metadata.tableReference;
-    
-    //** Delete matching line_items from original table
-    const query = `SELECT *
-                    FROM (
-                      SELECT
-                          *,
-                          ROW_NUMBER()
-                              OVER (PARTITION BY line_item_id)
-                              as rn
-                      FROM \`data-warehouse-srichand.${datasetId}.${tableId}\`
-                    ) as no_dup
-                    WHERE no_dup.rn = 1`;
+   
+    async function removeDuplicateFromTable(){
+      //** Delete matching line_items from original table
+      const query = `SELECT *
+                      FROM (
+                        SELECT
+                            *,
+                            ROW_NUMBER()
+                                OVER (PARTITION BY line_item_id)
+                                as rn
+                        FROM \`data-warehouse-srichand.${datasetId}.${tableId}\`
+                      ) as no_dup
+                      WHERE no_dup.rn = 1`;
 
-    // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
-    const options = {
-      query: query,
-      // Location must match that of the dataset(s) referenced in the query.
-      location: 'US',
-      writeDisposition: 'WRITE_TRUNCATE',
-      destinationTable: destinationTableRef,
-    };
+      // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+      const options = {
+        query: query,
+        // Location must match that of the dataset(s) referenced in the query.
+        location: 'US',
+        writeDisposition: 'WRITE_TRUNCATE',
+        destinationTable: destinationTableRef,
+      };
 
-    // Run the query as a job
-    const [job] = await bigquery.createQueryJob(options);
-    console.log(`Job ${job.id} start remove duplicate.`);
+      // Run the query as a job
+      const [job] = await bigquery.createQueryJob(options);
+      console.log(`Job ${job.id} start remove duplicate.`);
 
-    // Wait for the query to finish
-    const [rows] = await job.getQueryResults();
-    console.log(`Job ${job.id} complete delete duplicate from transactions table.`);
+      // Wait for the query to finish
+      const [rows] = await job.getQueryResults();
+      console.log(`Job ${job.id} complete delete duplicate from transactions table.`);
+
+      return rows
+    }
+
+    rows = await removeDuplicateFromTable();
 
     // remove row number columm
     const query2 = `SELECT * except (rn) FROM \`data-warehouse-srichand.${datasetId}.${tableId}\``;
@@ -74,7 +80,7 @@ async function main() {
     const [rows2] = await job2.getQueryResults();
     console.log(`Job ${job2.id} complete delete column row_number(rn).`);
 
-    return
+    return rows2
   }
   // [END bigquery_add_column_load_append]
   return await removeDuplicates();
